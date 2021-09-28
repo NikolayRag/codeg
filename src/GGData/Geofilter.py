f560b1cb765474b1bc51e6a5aeecdae4711d4f5b
NikolayRag <nikolayr@mail.ru>
2021-09-28 18:28:17 +0300

    comments

--- src/GGData/Geofilter.py
+++ src/GGData/Geofilter.py
@@ -11,17 +11,25 @@ class Geofilter():
 
 
 
+# Init filter with provided data dict to be private
 	def __init__(self, _data={}):
 		self.dataOwn = _data
 		self.dataPublic = {}
 
 
 
-	# return False, new geo, or True if provided geo is modified
+# Main proccess function for overload in derived filters.
+# Called from Geomark.
+#
+# Return:
+# False = geometry unmodified
+# True = geometry modified
+# (reserved) newGeo = totally new geometry
 	def proccess(self, _geo, _data):
 		return False
 
 
 
+# Return public output data, which is blank if not filled by .proccess()
 	def dataGet(self):
 		return dict(self.dataPublic)
