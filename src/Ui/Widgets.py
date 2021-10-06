09f5cbef19dd1741fca47778f0ff645af263d475
NikolayRag <nikolayr@mail.ru>
2021-10-06 17:49:11 +0300

    add ColorPicker widget

---
+++ src/Ui/Widgets.py
@@ -0,0 +1,42 @@
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+
+
+#thanks to https://stackoverflow.com/users/3336423/jpo38 at https://stackoverflow.com/questions/18257281/qt-color-picker-widget
+class ColorPicker(QPushButton):
+	
+	sigChanged = Signal(QColor)
+
+	color = None
+
+
+
+	def __init__(self, _color):
+		QPushButton.__init__(self)
+
+
+		self.setColor(_color)
+
+		self.clicked.connect(self.changeColor)
+
+
+
+	def changeColor(self):
+		newColor = QColorDialog.getColor(self.color, self.parentWidget())
+		if newColor != self.color:
+			self.setColor(newColor)
+
+			self.sigChanged.emit(newColor)
+
+
+
+	def setColor(self, _color):
+		self.color = _color
+		self.setStyleSheet( "background-color: " + self.color.name() )
+
+
+
+	def getColor(self):
+		return self.color
