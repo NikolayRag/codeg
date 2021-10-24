629c00400920a68740383dbfd2af940a642e3bbf
NikolayRag <nikolayr@mail.ru>
2021-10-24 17:04:38 +0300

    add simplify color picker

--- src/Ui/Widgets/ColorPicker.py
+++ src/Ui/Widgets/ColorPicker.py
@@ -20,23 +20,60 @@ class ColorPicker(QPushButton):
 
 		self.setColor(QColor(_color))
 
-		self.clicked.connect(self.changeColor)
+		self.clicked.connect(self.palettePop)
+
+
+		self.lPalette = QFrame()
+		self.lPalette.resize(214,100)
+		self.lPalette.setWindowFlags(Qt.Popup)
+
+		cLayout = QGridLayout(self.lPalette)
+		cLayout.setSpacing(0)
+		cLayout.setContentsMargins(0,0,0,0)
 
 
 
-	def changeColor(self):
-		newColor = QColorDialog.getColor(self.color, self.parentWidget())
+		def makeSample(_color):
+			cSample = QPushButton()
+			cSample.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
 
-		if newColor == QColor():
+			cColor = QColor(_color)
+			cSample.setStyleSheet(f"border:0; background:{cColor.name()}")
+
+			cSample.clicked.connect(lambda: self.picked(cColor))
+
+			return cSample
+
+
+		for iX in range(1,14):
+			for iY in range(1,7):
+				cSample = makeSample( QColor.fromHslF(iX/14.,1,iY/7.) )
+				cLayout.addWidget(cSample, iY, iX)
+
+
+		for iY in range(1,7):
+			cSample = makeSample( QColor.fromHslF(0,0,iY/7.) )
+			cLayout.addWidget(cSample, iY, 15)
+
+
+
+	def picked(self, _color):
+		self.lPalette.hide()
+
+
+		if _color.name() == self.color.name():
 			return
 
-		if newColor.name() == self.color.name():
-			return
+		self.setColor(_color)
+
+		self.sigChangedColor.emit(_color.name())
 
 
-		self.setColor(newColor)
 
-		self.sigChangedColor.emit(newColor.name())
+	def palettePop(self):
+		pos = self.mapToGlobal(QPoint(self.width()+2,0))
+		self.lPalette.move(pos)
+		self.lPalette.show()
 
 
 
