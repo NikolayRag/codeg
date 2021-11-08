79b65a813f9527993586ddd2b146d07673dad8c4
NikolayRag <nikolayr@mail.ru>
2021-11-09 02:39:56 +0300

    add PrefsWidget fills back ArgsBlock

--- src/Ui/PrefsWidget.py
+++ src/Ui/PrefsWidget.py
@@ -7,6 +7,8 @@ from PySide2.QtUiTools import *
 class PrefsWidget():
 	defUi = './Ui/PrefsWidget.ui'
 
+	links = {}
+
 
 
 	def __init__(self, _parent, _blocks):
@@ -20,24 +22,39 @@ class PrefsWidget():
 		for arg in _blocks:
 			wForm = QFormLayout()
 
-			flag = False
+
+			blockDict = {}
+
 			for prefN, prefV in arg._getData().items():
 				if not prefV:
 					continue
-				flag = True
 
 
 				fieldVal = QLineEdit(str(getattr(arg, prefN)))
 				wForm.addRow(prefV[2], fieldVal)
 
+				blockDict[prefN] = fieldVal
 
-			if flag:
+
+			if blockDict:
 				wGroup = QGroupBox(arg._getName())
 				wGroup.setLayout(wForm)
 				wLayPrefs.addWidget(wGroup)
+
+
+			self.links[arg] = blockDict
 
 
 
 	def exec(self):
 		if not self.prefsUi.exec():
 			return
+
+
+		for cBock, fields in self.links.items():
+			for fieldN, fieldV in fields.items():
+				cType = cBock._getData()[fieldN][1]
+				setattr(cBock, fieldN, cType(fieldV.text()))
+
+
+		return True
