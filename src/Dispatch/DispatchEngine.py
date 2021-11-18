76f199a47c6cb262d1fab12584f8444cd4656978
NikolayRag <nikolayr@mail.ru>
2021-11-18 06:25:23 +0300

    add store initial DispatchEngine name and size

--- src/Dispatch/DispatchEngine.py
+++ src/Dispatch/DispatchEngine.py
@@ -1,14 +1,17 @@
 class DispatchEngine():
-	name = 'Dummy'
+	nameRoot = ''
+	name = ''
 
+	sizeRoot = None
 	size = None
+
 	definition = None
 
 
 
 	def __init__(self, _name, _size, _definition=None):
-		self.name = _name
-		self.size = _size
+		self.nameRoot = self.name = _name
+		self.sizeRoot = self.size = _size
 		self.definition = _definition
 
 
