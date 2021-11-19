de4192d1583cbe0fe7b84897422c51cb141da4ec
NikolayRag <nikolayr@mail.ru>
2021-11-19 05:33:10 +0300

    add prototype DispatchEngine.enumerate()

--- src/Dispatch/DispatchEngine.py
+++ src/Dispatch/DispatchEngine.py
@@ -7,6 +7,16 @@ class DispatchEngine():
 
 	privData = None
 
+	instances = {}
+
+
+
+	### OVERLOAD ###
+
+	@classmethod
+	def enumerate(it, _defs=None):
+		return it.instances
+
 
 
 	def __init__(self, _name, _size, _privData=None):
