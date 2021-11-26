d72bff6bcd37ed761dce4121f3bd2b3a91999670
NikolayRag <nikolayr@mail.ru>
2021-11-26 16:13:22 +0300

    fix

--- src/Dispatch/DispatchManager.py
+++ src/Dispatch/DispatchManager.py
@@ -78,7 +78,7 @@ class DispatchManager():
 
 		def devOk(_dev, _ev):
 			if _dev.test():
-				_cb and _cb(_dev.getName(), True)
+				_cb and _cb(_dev.getName())
 
 				self.allDevices[_dev.getName()]= _dev
 
