7c24b951d6d78f37e511089b708f35f751f89add
NikolayRag <nikolayr@mail.ru>
2021-12-07 16:28:42 +0300

    change DispatchWidget to QObject

--- src/Ui/DispatchWidget.py
+++ src/Ui/DispatchWidget.py
@@ -1,7 +1,15 @@
-class DispatchWidget():
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+
+
+class DispatchWidget(QObject):
 
 
 	def __init__(self, _dispatch, _wRoot, _tracer=None):
+		QObject.__init__(self)
+
 		self.dispatch = _dispatch
 		self.wRoot = _wRoot
 		self.tracer = _tracer
